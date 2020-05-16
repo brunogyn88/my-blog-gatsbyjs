1772bace8dad0df0ba46202dfc76b1c41c4a95be
Bruno Alves <brunogyn.eng@gmail.com>
2020-05-16 11:40:52 -0300

    criação do component Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Matt Foo</h1>
+    <h2>Programador Backend</h2>
+    <p>
+      Officia non reprehenderit dolor ex qui sit minim non ex nisi reprehenderit
+      amet mollit ut.
+    </p>
+  </div>
+)
+
+export default Profile
