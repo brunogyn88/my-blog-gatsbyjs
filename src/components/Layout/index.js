f801d6e71167d6ffd9fa97a8cd83c9743c62796d
Bruno Alves <brunogyn.eng@gmail.com>
2020-05-16 15:47:17 -0300

    Adicionando link no sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Profile from "../Profile"
+import Sidebar from "../Sidebar"
 
-import * as S from './styled'
+import * as S from "./styled"
 import GlobalStyles from "../../styles/global"
 
 const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <aside>
-        <Profile />
-      </aside>
+      <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
     </S.LayoutWrapper>
   )
